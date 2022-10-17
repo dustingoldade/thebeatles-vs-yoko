36b57affb038c01f7b19606a553cdc4448174740
Dustin Goldade <dagoldade@gmail.com>
2022-10-17 17:35:15 +0200

    Post custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -10,162 +10,178 @@ import LettersToDisplay from "./components/lettersToDisplay/LettersToDisplay";
 import Keyboard from "./components/keyboard/Keyboard";
 import NewGameBtn from "./components/newGameBtn/NewGameBtn";
 import jsonText from "./locales/en.json";
+import useGamePlay from "./useGamePlay";
 const { New_Game_Button_Label } = jsonText;
 
 const App = () => {
-  const [blockAllLetters, setBlockAllLetters] = useState(true);
-  const [isModal, setIsModal] = useState(false);
-  const [winModalText, setWinModalText] = useState(true);
-  const [activePhrase, setActivePhrase] = useState("");
-  const [guessedLettersArray, setGuessedLettersArray] = useState([]);
-  const [lettersToDisplay, setLettersToDisplay] = useState([]);
-  const [scoreboardArray, setScoreBoardArray] = useState(["_", "_", "_", "_"]);
-  const [scoreBoardGridSizes, setScoreBoardGridSizes] = useState({});
-  const [activeImgs, setActiveImgs] = useState({
-    activeYokoImg: yokoImages[0],
-    activeBeatlesImg: beatlesImages[0],
-  });
-  const [winLossScores, setWinLossScored] = useState({
-    theBeatles: 0,
-    yoko: 0,
-  });
+  const {
+    blockAllLetters,
+    isModal,
+    winModalText,
+    activePhrase,
+    guessedLettersArray,
+    lettersToDisplay,
+    scoreboardArray,
+    scoreBoardGridSizes,
+    activeImgs,
+    winLossScores,
+    newGame,
+    checkGuessedLetter,
+  } = useGamePlay();
 
-  /////////////////////////
-  //// Game Play Logic ////
-  /////////////////////////
+  // const [blockAllLetters, setBlockAllLetters] = useState(true);
+  // const [isModal, setIsModal] = useState(false);
+  // const [winModalText, setWinModalText] = useState(true);
+  // const [activePhrase, setActivePhrase] = useState("");
+  // const [guessedLettersArray, setGuessedLettersArray] = useState([]);
+  // const [lettersToDisplay, setLettersToDisplay] = useState([]);
+  // const [scoreboardArray, setScoreBoardArray] = useState(["_", "_", "_", "_"]);
+  // const [scoreBoardGridSizes, setScoreBoardGridSizes] = useState({});
+  // const [activeImgs, setActiveImgs] = useState({
+  //   activeYokoImg: yokoImages[0],
+  //   activeBeatlesImg: beatlesImages[0],
+  // });
+  // const [winLossScores, setWinLossScored] = useState({
+  //   theBeatles: 0,
+  //   yoko: 0,
+  // });
 
-  const newGame = () => {
-    setBlockAllLetters(false);
+  // /////////////////////////
+  // //// Game Play Logic ////
+  // /////////////////////////
 
-    //Reset the scoreboard and the letters that have been guessed
-    setGuessedLettersArray([]);
-    setScoreBoardArray([0, 0, 0, 0]);
+  // const newGame = () => {
+  //   setBlockAllLetters(false);
 
-    // Pick new photos -- used in ScoreBoard Component and pick a new random phrase
-    newPhotos();
-    setActivePhrase(returnRandomItemInArray(phrasesToGuess));
-    setIsModal(false);
-  };
+  //   //Reset the scoreboard and the letters that have been guessed
+  //   setGuessedLettersArray([]);
+  //   setScoreBoardArray([0, 0, 0, 0]);
 
-  const checkGuessedLetter = (letter) => {
-    setGuessedLettersArray((prevState) => [letter, ...prevState]);
-  };
-  ////////////////////////////////////////////////////////////////////
+  //   // Pick new photos -- used in ScoreBoard Component and pick a new random phrase
+  //   newPhotos();
+  //   setActivePhrase(returnRandomItemInArray(phrasesToGuess));
+  //   setIsModal(false);
+  // };
 
-  // Check if the game is won or loss:
-  useEffect(() => {
-    if (guessedLettersArray.length < 1) return;
+  // const checkGuessedLetter = (letter) => {
+  //   setGuessedLettersArray((prevState) => [letter, ...prevState]);
+  // };
+  // ////////////////////////////////////////////////////////////////////
 
-    activePhrase.includes(guessedLettersArray[0])
-      ? correctGuess()
-      : incorrectGuess();
+  // // Check if the game is won or loss:
+  // useEffect(() => {
+  //   if (guessedLettersArray.length < 1) return;
 
-    // On Win
-    if (checkIfWin(activePhrase, guessedLettersArray)) {
-      runWinFunctions();
-    }
-    // On Loss
-    if (checkIfLoss()) {
-      runLossFunctions();
-    }
-  }, [guessedLettersArray]);
+  //   activePhrase.includes(guessedLettersArray[0])
+  //     ? correctGuess()
+  //     : incorrectGuess();
 
-  const checkIfWin = (activePhrase, guessedLetters) => {
-    for (let i in activePhrase) {
-      const activeLetter = activePhrase[i];
-      if (activeLetter === " ") {
-        continue;
-      } //Needed to skip over spaces in the active phrase (occurs when phrases are more than one word)
-      if (!guessedLetters.includes(activeLetter)) {
-        return false;
-      }
-    }
-    return true;
-  };
+  //   // On Win
+  //   if (checkIfWin(activePhrase, guessedLettersArray)) {
+  //     runWinFunctions();
+  //   }
+  //   // On Loss
+  //   if (checkIfLoss()) {
+  //     runLossFunctions();
+  //   }
+  // }, [guessedLettersArray]);
 
-  const checkIfLoss = () => {
-    if (scoreboardArray[0] === -1) return true;
-    return false;
-  };
+  // const checkIfWin = (activePhrase, guessedLetters) => {
+  //   for (let i in activePhrase) {
+  //     const activeLetter = activePhrase[i];
+  //     if (activeLetter === " ") {
+  //       continue;
+  //     } //Needed to skip over spaces in the active phrase (occurs when phrases are more than one word)
+  //     if (!guessedLetters.includes(activeLetter)) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // };
 
-  const correctGuess = () => {
-    for (let i in scoreboardArray) {
-      if (scoreboardArray[i] !== 1) {
-        scoreboardArray[i] = 1;
-        break;
-      }
-    }
-  };
+  // const checkIfLoss = () => {
+  //   if (scoreboardArray[0] === -1) return true;
+  //   return false;
+  // };
 
-  const incorrectGuess = () => {
-    for (let i in scoreboardArray) {
-      const reverseIndex = scoreboardArray.length - 1 - i;
-      if (scoreboardArray[reverseIndex] !== -1) {
-        scoreboardArray[reverseIndex] = -1;
-        return;
-      }
-    }
-  };
+  // const correctGuess = () => {
+  //   for (let i in scoreboardArray) {
+  //     if (scoreboardArray[i] !== 1) {
+  //       scoreboardArray[i] = 1;
+  //       break;
+  //     }
+  //   }
+  // };
 
-  const runWinFunctions = () => {
-    setWinModalText(true);
-    setIsModal(true);
-    setWinLossScored((prevState) => {
-      return { ...prevState, theBeatles: prevState.theBeatles++ };
-    });
-  };
+  // const incorrectGuess = () => {
+  //   for (let i in scoreboardArray) {
+  //     const reverseIndex = scoreboardArray.length - 1 - i;
+  //     if (scoreboardArray[reverseIndex] !== -1) {
+  //       scoreboardArray[reverseIndex] = -1;
+  //       return;
+  //     }
+  //   }
+  // };
 
-  const runLossFunctions = () => {
-    setWinModalText(false);
-    setIsModal(true);
-    setWinLossScored((prevState) => {
-      return { ...prevState, yoko: prevState.yoko++ };
-    });
-  };
+  // const runWinFunctions = () => {
+  //   setWinModalText(true);
+  //   setIsModal(true);
+  //   setWinLossScored((prevState) => {
+  //     return { ...prevState, theBeatles: prevState.theBeatles++ };
+  //   });
+  // };
 
-  //######################################
+  // const runLossFunctions = () => {
+  //   setWinModalText(false);
+  //   setIsModal(true);
+  //   setWinLossScored((prevState) => {
+  //     return { ...prevState, yoko: prevState.yoko++ };
+  //   });
+  // };
 
-  /////////////////////////
-  ///// Display Logic /////
-  /////////////////////////
+  // //######################################
 
-  const newPhotos = () => {
-    setActiveImgs({
-      activeYokoImg: returnRandomItemInArray(yokoImages),
-      activeBeatlesImg: returnRandomItemInArray(beatlesImages),
-    });
-  };
+  // /////////////////////////
+  // ///// Display Logic /////
+  // /////////////////////////
 
-  useEffect(() => {
-    const tempArray = [];
-    for (const letter of activePhrase) {
-      if (guessedLettersArray.includes(letter)) {
-        tempArray.push(letter);
-        {
-          continue;
-        }
-      }
-      if (letter === " ") {
-        tempArray.push(" ");
-      } else {
-        tempArray.push("_");
-      }
-    }
-    setLettersToDisplay(tempArray);
-  }, [guessedLettersArray, activePhrase]);
+  // const newPhotos = () => {
+  //   setActiveImgs({
+  //     activeYokoImg: returnRandomItemInArray(yokoImages),
+  //     activeBeatlesImg: returnRandomItemInArray(beatlesImages),
+  //   });
+  // };
 
-  //Sets the width of the three different Grid items that occupy the scoreboard and hold either a photo of the Beatles, blank, or Yoko Ono
-  useEffect(() => {
-    let leftGrid = returnGridSize(scoreboardArray, 1);
-    let rightGrid = returnGridSize(scoreboardArray, -1);
-    let centerGrid = 12 - leftGrid - rightGrid;
-    setScoreBoardGridSizes({
-      leftGrid: leftGrid,
-      rightGrid: rightGrid,
-      centerGrid: centerGrid,
-    });
-  }, [guessedLettersArray]);
-  //######################################
+  // useEffect(() => {
+  //   const tempArray = [];
+  //   for (const letter of activePhrase) {
+  //     if (guessedLettersArray.includes(letter)) {
+  //       tempArray.push(letter);
+  //       {
+  //         continue;
+  //       }
+  //     }
+  //     if (letter === " ") {
+  //       tempArray.push(" ");
+  //     } else {
+  //       tempArray.push("_");
+  //     }
+  //   }
+  //   setLettersToDisplay(tempArray);
+  // }, [guessedLettersArray, activePhrase]);
+
+  // //Sets the width of the three different Grid items that occupy the scoreboard and hold either a photo of the Beatles, blank, or Yoko Ono
+  // useEffect(() => {
+  //   let leftGrid = returnGridSize(scoreboardArray, 1);
+  //   let rightGrid = returnGridSize(scoreboardArray, -1);
+  //   let centerGrid = 12 - leftGrid - rightGrid;
+  //   setScoreBoardGridSizes({
+  //     leftGrid: leftGrid,
+  //     rightGrid: rightGrid,
+  //     centerGrid: centerGrid,
+  //   });
+  // }, [guessedLettersArray]);
+  // //######################################
   return (
     <>
       {isModal && (
---
+++ src/useGamePlay.jsx
@@ -0,0 +1,174 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { beatlesImages, yokoImages } from "./img/imgIndex";
+import { phrasesToGuess } from "./helpers/data";
+import { returnRandomItemInArray, returnGridSize } from "./helpers/utils";
+
+const useGamePlay = () => {
+  const [blockAllLetters, setBlockAllLetters] = useState(true);
+  const [isModal, setIsModal] = useState(false);
+  const [winModalText, setWinModalText] = useState(true);
+  const [activePhrase, setActivePhrase] = useState("");
+  const [guessedLettersArray, setGuessedLettersArray] = useState([]);
+  const [lettersToDisplay, setLettersToDisplay] = useState([]);
+  const [scoreboardArray, setScoreBoardArray] = useState(["_", "_", "_", "_"]);
+  const [scoreBoardGridSizes, setScoreBoardGridSizes] = useState({});
+  const [activeImgs, setActiveImgs] = useState({
+    activeYokoImg: yokoImages[0],
+    activeBeatlesImg: beatlesImages[0],
+  });
+  const [winLossScores, setWinLossScored] = useState({
+    theBeatles: 0,
+    yoko: 0,
+  });
+
+  /////////////////////////
+  //// Game Play Logic ////
+  /////////////////////////
+
+  const newGame = () => {
+    setBlockAllLetters(false);
+
+    //Reset the scoreboard and the letters that have been guessed
+    setGuessedLettersArray([]);
+    setScoreBoardArray([0, 0, 0, 0]);
+
+    // Pick new photos -- used in ScoreBoard Component and pick a new random phrase
+    newPhotos();
+    setActivePhrase(returnRandomItemInArray(phrasesToGuess));
+    setIsModal(false);
+  };
+
+  const checkGuessedLetter = (letter) => {
+    setGuessedLettersArray((prevState) => [letter, ...prevState]);
+  };
+
+  // Check if the game is won or loss:
+  useEffect(() => {
+    if (guessedLettersArray.length < 1) return;
+
+    activePhrase.includes(guessedLettersArray[0])
+      ? correctGuess()
+      : incorrectGuess();
+
+    // On Win
+    if (checkIfWin(activePhrase, guessedLettersArray)) {
+      runWinFunctions();
+    }
+    // On Loss
+    if (checkIfLoss()) {
+      runLossFunctions();
+    }
+  }, [guessedLettersArray]);
+
+  const checkIfWin = (activePhrase, guessedLetters) => {
+    for (let i in activePhrase) {
+      const activeLetter = activePhrase[i];
+      if (activeLetter === " ") {
+        continue;
+      } //Needed to skip over spaces in the active phrase (occurs when phrases are more than one word)
+      if (!guessedLetters.includes(activeLetter)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const checkIfLoss = () => {
+    if (scoreboardArray[0] === -1) return true;
+    return false;
+  };
+
+  const correctGuess = () => {
+    for (let i in scoreboardArray) {
+      if (scoreboardArray[i] !== 1) {
+        scoreboardArray[i] = 1;
+        break;
+      }
+    }
+  };
+
+  const incorrectGuess = () => {
+    for (let i in scoreboardArray) {
+      const reverseIndex = scoreboardArray.length - 1 - i;
+      if (scoreboardArray[reverseIndex] !== -1) {
+        scoreboardArray[reverseIndex] = -1;
+        return;
+      }
+    }
+  };
+
+  const runWinFunctions = () => {
+    setWinModalText(true);
+    setIsModal(true);
+    setWinLossScored((prevState) => {
+      return { ...prevState, theBeatles: prevState.theBeatles++ };
+    });
+  };
+
+  const runLossFunctions = () => {
+    setWinModalText(false);
+    setIsModal(true);
+    setWinLossScored((prevState) => {
+      return { ...prevState, yoko: prevState.yoko++ };
+    });
+  };
+
+  /////////////////////////
+  ///// Display Logic /////
+  /////////////////////////
+
+  const newPhotos = () => {
+    setActiveImgs({
+      activeYokoImg: returnRandomItemInArray(yokoImages),
+      activeBeatlesImg: returnRandomItemInArray(beatlesImages),
+    });
+  };
+
+  useEffect(() => {
+    const tempArray = [];
+    for (const letter of activePhrase) {
+      if (guessedLettersArray.includes(letter)) {
+        tempArray.push(letter);
+        {
+          continue;
+        }
+      }
+      if (letter === " ") {
+        tempArray.push(" ");
+      } else {
+        tempArray.push("_");
+      }
+    }
+    setLettersToDisplay(tempArray);
+  }, [guessedLettersArray, activePhrase]);
+
+  //Sets the width of the three different Grid items that occupy the scoreboard and hold either a photo of the Beatles, blank, or Yoko Ono
+  useEffect(() => {
+    let leftGrid = returnGridSize(scoreboardArray, 1);
+    let rightGrid = returnGridSize(scoreboardArray, -1);
+    let centerGrid = 12 - leftGrid - rightGrid;
+    setScoreBoardGridSizes({
+      leftGrid: leftGrid,
+      rightGrid: rightGrid,
+      centerGrid: centerGrid,
+    });
+  }, [guessedLettersArray]);
+
+  return {
+    blockAllLetters,
+    isModal,
+    winModalText,
+    activePhrase,
+    guessedLettersArray,
+    lettersToDisplay,
+    scoreboardArray,
+    scoreBoardGridSizes,
+    activeImgs,
+    winLossScores,
+    newGame,
+    checkGuessedLetter,
+  };
+};
+
+export default useGamePlay;
